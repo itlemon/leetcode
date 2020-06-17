f71d5e1f5d67193ebb9ef964906cd9cccf3e0534
itlemon <lemon_jiang@aliyun.com>
2020-06-17 11:21:14 +0800

    【title】leetcode 初始化数组题目
    description：leetcode 初始化数组题目

---
+++ array/src/main/java/cn/itlemon/leetcode/array/No11ContainerWithMostWater.java
@@ -0,0 +1,36 @@
+package cn.itlemon.leetcode.array;
+
+/**
+ * 11. 盛最多水的容器
+ * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ * <p>
+ * 说明：你不能倾斜容器，且 n 的值至少为 2。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：[1,8,6,2,5,4,8,3,7]
+ * 输出：49
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/container-with-most-water
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author jiangpingping
+ * @date 2020/6/17 10:37
+ */
+public class No11ContainerWithMostWater {
+
+    public int maxArea(int[] height) {
+        int maxArea = 0, start = 0, end = height.length - 1;
+        while (start < end) {
+            maxArea = Math.max(maxArea, Math.min(height[start], height[end]) * (end - start));
+            if (height[start] < height[end]) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+        return maxArea;
+    }
+
+}
---
+++ array/src/main/java/cn/itlemon/leetcode/array/No4MedianOfTwoSortedArrays.java
@@ -0,0 +1,36 @@
+package cn.itlemon.leetcode.array;
+
+/**
+ * 4. 寻找两个正序数组的中位数
+ * 给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。
+ * <p>
+ * 请你找出这两个正序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+ * <p>
+ * 你可以假设 nums1 和 nums2 不会同时为空。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ * <p>
+ * 则中位数是 2.0
+ * 示例 2:
+ * <p>
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ * <p>
+ * 则中位数是 (2 + 3)/2 = 2.5
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/median-of-two-sorted-arrays
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author jiangpingping
+ * @date 2020/6/17 10:31
+ */
+public class No4MedianOfTwoSortedArrays {
+
+
+}
