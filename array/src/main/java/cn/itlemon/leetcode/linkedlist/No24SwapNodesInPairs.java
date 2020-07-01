2ef6d52e85db711100af1cb915498869491175ab
itlemon <lemon_jiang@aliyun.com>
2020-07-01 21:05:06 +0800

    【title】leetcode 24号两两交换链表中的节点
    description：leetcode 24号两两交换链表中的节点

---
+++ array/src/main/java/cn/itlemon/leetcode/linkedlist/No24SwapNodesInPairs.java
@@ -0,0 +1,34 @@
+package cn.itlemon.leetcode.linkedlist;
+
+import cn.itlemon.leetcode.model.ListNode;
+
+/**
+ * 24. 两两交换链表中的节点
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ * <p>
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 1->2->3->4, 你应该返回 2->1->4->3.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author jiangpingping
+ * @date 2020/7/1 18:55
+ */
+public class No24SwapNodesInPairs {
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
+    }
+
+}
