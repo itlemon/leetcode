fe61f3a569d275d48f0f90a3157d9d651781aee7
itlemon <lemon_jiang@aliyun.com>
2020-07-01 21:11:22 +0800

    【title】leetcode 24号两两交换链表中的节点
    description：leetcode 24号两两交换链表中的节点

---
+++ array/src/main/java/cn/itlemon/leetcode/model/ListNode.java
@@ -0,0 +1,16 @@
+package cn.itlemon.leetcode.model;
+
+/**
+ * @author jiangpingping
+ * @date 2020/7/1 11:24
+ */
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+
+}
